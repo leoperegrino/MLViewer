8d3ca12d6e6c63958691b5309219acb62e135bdb
leoperegrino <leo.peregrino@hotmail.com>
2021-05-24 23:44:27 -0300

    logistic model

--- src/Models.py
+++ src/Models.py
@@ -1,6 +1,7 @@
 #!/bin/python
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from pygame.surfarray import pixels2d
 from pygame.colordict import THECOLORS as COLOR
 
@@ -16,6 +17,7 @@ class Data():
     def append(self, value):
         self.Y[:self.size - 1, 0] = self.Y[1:self.size + 1, 0]
         self.Y[self.size - 1, 0] = value
+
 
 class Model(Data):
     """
@@ -43,6 +45,7 @@ class Model(Data):
 
         del pixels
 
+
 class Linear(Model):
     """
     Linear Regression model
@@ -56,3 +59,11 @@ class Linear(Model):
         self.stats_['Intercept'] = float(self.model.intercept_)
         self.stats_['Score'] = float(self.model.score(self.X, self.Y))
         return self.stats_
+
+
+class Logistic(Model):
+    """
+    Logistic Regression model
+    """
+    def __init__(self, bg):
+       super().__init__(bg, LogisticRegression())
